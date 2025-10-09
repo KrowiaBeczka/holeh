0c7f8a17b96c5ca362eba5237ed4f13f97ef97df
bobs <97963666+KrowiaBeczka@users.noreply.github.com>
2025-10-09 23:35:56 +0200

    Update lib.lua

--- lib.lua
+++ lib.lua
@@ -2114,9 +2114,8 @@ function library:init()
                     })
 
                     objs.optionholder = utility:Draw('Square',{
-                    Size = newUDim2(1, 0, 1, -15);
-                    Position = newUDim2(0, 0, 0, 13);
-
+                        Size = newUDim2(1-.03,0,1,-15);
+                        Position = newUDim2(.015,0,0,13);
                         Transparency = 0;
                         ZIndex = z+1;
                         Parent = objs.background;
